0731389d8133b03afe574d449ee1d5b0070467cf
romulo lima <dhb.tecnologia@gmail.com>
2021-05-15 21:50:55 -0300

    Script para mineração de bitcoins

---
+++ minerar.py
@@ -0,0 +1,36 @@
+"""
+Codigo Para Minerar bitcoin utilizando Python
+"""
+
+from hashlib import sha256
+import time
+
+
+def aplicar_sha256(texto):
+    return sha256(texto.encode("ascii")).hexdigest()
+
+
+def minerar(num_bloco, transacoes, hash_anterior, qtde_zeros):
+    nonce = 0
+    while True:
+        texto = str(num_bloco) + transacoes + hash_anterior + str(nonce)
+        meu_hash = aplicar_sha256(texto)
+        if meu_hash.startswith("0" * qtde_zeros):
+            return nonce, meu_hash
+        nonce += 1
+
+
+if __name__ == "__main__":
+    num_bloco = 20
+    transacoes = """
+    Romulo->Diogo->15
+    Diogo->Heitor->10
+    Heitor->Bernard->5"""
+    qtde_zeros = 5 # Cuidado ao Alterar!!!
+    hash_anterior = "abc"
+    inicio = time.time()
+    resultado = minerar(num_bloco, transacoes, hash_anterior, qtde_zeros)
+    print(resultado)
+    print(time.time() - inicio)
+
+
